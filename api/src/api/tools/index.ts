7e7d55ebd0411c3df267a49635db2e8258990e25
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-06-24 03:04:08 -0300

    use delete tool

--- api/src/api/tools/index.ts
+++ api/src/api/tools/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import getTools from './getTools'
 import createTool from './postTools'
+import deleteTool from './deleteTool'
 
 const router = express.Router()
 
 router.get('/', getTools)
 router.post('/', createTool)
+router.delete('/', deleteTool)
 
 export default router
