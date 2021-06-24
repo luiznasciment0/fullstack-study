be2bc40f36032870643dae660bd158c7284c2789
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-06-24 01:25:05 -0300

    create getToolsByTag query

---
+++ api/src/api/tools/getToolsByTag.ts
@@ -0,0 +1,23 @@
+import { Response } from 'express'
+import { pool } from '../../index'
+
+type ToolsByTag = {
+  res: Response
+  tag: string
+}
+
+const getToolsByTag = ({ res, tag }: ToolsByTag) => {
+  try {
+    pool.query('SELECT * FROM toolsv3 WHERE $1 = ANY (tags)', [tag], (error, results) => {
+      if (error) {
+        throw error
+      }
+  
+      res.status(200).json(results?.rows)
+    })
+  } catch (error) {
+    res.status(404)
+  }
+}
+
+export default getToolsByTag
