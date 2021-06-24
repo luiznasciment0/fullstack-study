88529554cc7d1738d58b6e6e32299a80fa728cd0
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-06-24 01:25:29 -0300

    organize index file

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,10 @@
-import express, { Request, Response } from 'express'
 import pg from 'pg'
-import dotenv from 'dotenv'
-
-const app = express()
-const port = 3000
-dotenv.config()
+import { config } from './config'
+import { app } from './app'
 
 const Pool = pg.Pool
 
-const pool = new Pool({
+export const pool = new Pool({
   user: process.env.DB_USER, 
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
@@ -16,19 +12,6 @@ const pool = new Pool({
   port: Number(process.env.DB_PORT) || 5432,
 })
 
-const getTools = (_req: Request, res: Response) => {
-  pool.query('SELECT * FROM toolsv3 ORDER BY id ASC', (error, results) => {
-    if (error) {
-      console.log('database error', error)
-      throw error
-    }
-
-    res.status(200).json(results?.rows)
-  }) 
-}
-
-app.get('/', getTools)
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+app.listen(config.PORT, () => {
+  console.log(`App listening at http://localhost:${config.PORT}`)
 })
