5f523256a54b7910024f47418bb9004821d37a7a
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-06-24 02:57:53 -0300

    use parsers

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,8 +2,10 @@ import express from 'express'
 import tools from './api/tools'
 
 export const app = express()
+const urlParser = express.urlencoded({ extended: true })
+const jsonParser = express.json()
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(urlParser)
+app.use(jsonParser)
 // routes
 app.use('/tools', tools)
