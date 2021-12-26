c3cd60f4d054a139ab3166240856cbf93a399b69
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-12-26 15:35:50 -0300

    create getUserById method

--- nodejs/src/controllers/UserController.js
+++ nodejs/src/controllers/UserController.js
@@ -15,4 +15,16 @@ module.exports = {
     response.writeHead(200, { "Content-Type": "application/json" });
     response.end(JSON.stringify(sortedUsers));
   },
+  getUserById(request, response) {
+    const { id } = request.params;
+    const user = users.find((user) => user.id === Number(id));
+
+    if (!user) {
+      response.writeHead(404, { "Content-Type": "application/json" });
+      response.end(`User ${id} Was Not found`);
+    } else {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify({ user }));
+    }
+  },
 };
