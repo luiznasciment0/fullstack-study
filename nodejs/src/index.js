282dd99f72516a4d759bbb4cec4ce0dbc777ffe4
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-12-25 22:31:09 -0300

    use route

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,12 +1,18 @@
 const http = require("http");
-const users = require("./mocks/users");
+const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
-  if (request.url === "/users" && request.method === "GET") {
-    response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(JSON.stringify(users));
+  const route = routes.find((routeObject) => {
+    return (
+      routeObject.endpoint === request.url &&
+      routeObject.method === request.method
+    );
+  });
+
+  if (route) {
+    route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
     response.end(`Cannot ${request.method} ${request.url}`);
