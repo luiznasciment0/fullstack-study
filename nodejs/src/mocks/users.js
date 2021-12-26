b682f1a3e9d635badb5ac473ccae758181fcec9f
Luiz Paulo Nascimento <luizp.nascimento@outlook.com>
2021-12-25 22:18:47 -0300

    simple endpoint

---
+++ nodejs/src/mocks/users.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Luiz",
+  },
+  {
+    id: 2,
+    name: "Joao",
+  },
+  {
+    id: 3,
+    name: "Claudio",
+  },
+  {
+    id: 4,
+    name: "Israel",
+  },
+  {
+    id: 5,
+    name: "Lucas",
+  },
+];
